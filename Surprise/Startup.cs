2a6710ef9d418e969cf41950a4e69ad578aaafb7
RitzAnthony <anthony.ritz@students.hevs.ch>
2018-05-18 14:31:27 +0200

    Add project files.

---
+++ Surprise/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Surprise.Startup))]
+namespace Surprise
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
